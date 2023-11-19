e44f2e9bd9837a4ebe618f9f87e35c421b6203f3
Louis Teitelbaum <louis@teitelbaum.us>
2023-11-19 18:25:12 +0200

    dataviz initial

---
+++ _common.R
@@ -0,0 +1,12 @@
+set.seed(2023)
+
+library(tidyverse)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  # cache = TRUE,
+  fig.retina = 2,
+  fig.width = 8
+  # fig.show = "hold"
+)
